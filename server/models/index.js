962c07e7a9482f4bed0f317c1496df03b0e13b1b
Oriol Cervantes Sánchez <oriolcesa@hotmail.com>
2022-02-14 17:45:05 +0100

    Created envexample file

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,9 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const USER = process.env.DATABASE_USER;
+const NAME = process.env.DATABASE_NAME;
+const PASSWORD = process.env.DATABASE_PASSWORD;
 
 const config = {
   host: "localhost",
@@ -6,7 +11,7 @@ const config = {
   port: 5432
 }
 
-const sequelize = new Sequelize("localydb", "postgres", "codecloudnine", config);
+const sequelize = new Sequelize(NAME, USER, PASSWORD, config);
 
 
 module.exports = sequelize;
